b33c1e217c76f778a47017402ead83a450baaf6f
Pinak oza <pinakoza1999@gmail.com>
2025-02-25 11:08:46 +0530

    update SegmentationDataset

--- dataset.py
+++ dataset.py
@@ -1,35 +1,49 @@
-import os
-from torch.utils.data import Dataset
+import albumentations as albu
+
+from pathlib import Path
+from typing import Dict, List, Tuple, Any
 import numpy as np
-from helper_functions.utils.image_utils import (
-    load_rgb,
-    load_grayscale
-    )
+import torch
+from torch.utils.data import Dataset
+from pytorch_toolbelt.utils.torch_utils import image_to_tensor
+from helper_functions.utils.image_utils import load_rgb, load_grayscale
 
 class SegmentationDataset(Dataset):
-    
-    def __init__(self, image_dir, mask_dir, transform=None):
-        self.image_dir = image_dir
-        self.mask_dir = mask_dir
+    def __init__(
+        self,
+        samples: List[Tuple[Path, Path]],
+        transform: albu.Compose,
+        length: int = None,
+    ) -> None:
+        self.samples = samples
         self.transform = transform
-        self.images = os.listdir(self.image_dir)
 
-    def __len__(self):
-        return len(self.images)
+        if length is None:
+            self.length = len(self.samples)
+        else:
+            self.length = length
 
-    def __getitem__(self, index):
-        
-        img_path = os.path.join(self.image_dir, self.images[index])
-        mask_path = os.path.join(self.mask_dir, self.images[index].replace('.jpg', '.png'))
-        
-        image = load_rgb(img_path)
+    def __len__(self) -> int:
+        return self.length
+
+    def __getitem__(self, idx: int) -> Dict[str, Any]:
+        idx = idx % len(self.samples)
+
+        image_path, mask_path = self.samples[idx]
+
+        image = load_rgb(image_path)
         mask = load_grayscale(mask_path)
-        
-        mask[mask == 255.0] = 1.0
 
-        if self.transform is not None:
-            augmentations = self.transform(image=image, mask=mask)
-            image = augmentations["image"]
-            mask = augmentations["mask"]
+        # apply augmentations
+        sample = self.transform(image=image, mask=mask)
+        image, mask = sample["image"], sample["mask"]
 
-        return image, mask
+        mask = (mask > 0).astype(np.uint8)
+
+        mask = torch.from_numpy(mask)
+
+        return {
+            "image_id": image_path.stem,
+            "features": image_to_tensor(image),
+            "masks": torch.unsqueeze(mask, 0).float(),
+        }
